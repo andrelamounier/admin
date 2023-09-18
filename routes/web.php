3ad7d1c9959c936cba4fb4f3721a1046bdca1618
André Lamounier <0k0n4n@gmail.com>
2023-09-18 10:01:56 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -89,4 +89,5 @@ Route::middleware([
     
     Route::post('/add_cartao', [Forma_pagController::class,'add_cartao']);
     Route::get('/financeiro/cartao', [Forma_pagController::class,'cartao']);
+    
 });
