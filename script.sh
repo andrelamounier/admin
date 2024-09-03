79b7b0aea208c7dacc84e12cde500664b8239e5e
andre.lamounier <0k0n4n@gmail.com>
2024-09-03 08:05:12 -0300

    .

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/php /home/u446731902/domains/tiranossaurorex.com.br/admin/teste.php
+/usr/bin/php /home/u446731902/domains/tiranossaurorex.com.br/public_html/admin/artisan schedule:run >> /dev/null 2>&1
