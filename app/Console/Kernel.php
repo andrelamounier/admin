7b716aced7b71f53081116a89831db48cfd43f48
andre.lamounier <0k0n4n@gmail.com>
2024-09-12 11:31:57 -0300

    .

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,8 +19,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        //$schedule->command('email:vencimento')->dailyAt('08:00');
-        $schedule->command('email:vencimento')->everyMinute();
+        $schedule->command('email:vencimento')->dailyAt('08:00');
+        $schedule->command('email:vencimento')->dailyAt('08:30');
 
     }
 
